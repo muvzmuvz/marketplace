f3c7f8acbde8319a431b04dfa97354b06c82be12
ripper <rippergods@gmail.com>
2025-06-05 11:47:48 +0300

    исправил dto  у отзывов

--- ModelsDto/ReviewResponseDto.cs
+++ ModelsDto/ReviewResponseDto.cs
@@ -7,7 +7,7 @@ public class ReviewResponseDto
     public int Id { get; set; }
     public string Description { get; set; }
     public int UserId { get; set; }
-    public UserDto User { get; set; }
+    public UserResponseDto User { get; set; }
     public int ProductId { get; set; }
     public DateTime DateCreated { get; set; } = DateTime.UtcNow;
     public int ProductEvaluation { get; set; }
